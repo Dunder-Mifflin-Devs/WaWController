d3aa8d0ca85b2d97819a693c49e090bab932c3c7
Albertus Chua <albertus.chua@gmail.com>
2023-09-15 20:00:15 -0400

    Added props file and packages to allow development and production environments

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const app = express();
-const yn = require('yn');
 const port = process.env.PORT || 3000;
+const props = require('./config/props');
 
 require('dotenv').config({ path: './config/.env' });
 
 
-// Check if in a local environment
-const isMockEnvironment = yn(process.env.ENVIRONMENT === 'local');
-
-let dbUrl;
-if (isMockEnvironment) {
-    // If in mock env, use mock DB
-    dbUrl = process.env.MOCK_DB_URL;
-} else {
-    // Else use live DB
-    dbUrl = process.env.LIVE_DB_URL;
-}
-
 app.listen(port, () => {
-    console.log(`Server is running in ${port}.`);
-    console.log(`Database is on ${dbUrl}`)
+    console.log(`Running in a ${props.env} environment`);
+    console.log(`Server is running in port ${port}.`);
 });
