4f2c2c5acc0e81714b156107aa42263a5e6a75b9
Jennie <jenanemone@gmail.com>
2023-10-02 13:29:38 -0500

    filling in server and moving passport to main config

--- server.js
+++ server.js
@@ -2,8 +2,48 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const props = require('./config/props');
-
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
 require('dotenv').config({ path: './config/.env' });
+
+// Passport config
+require("./config/passport")(passport);
+
+//Connect To Database
+connectDB();
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Use flash messages for errors, info, etc...
+app.use(flash());
 
 /* TODO wide open routes go here*/
 
@@ -11,8 +51,12 @@ require('dotenv').config({ path: './config/.env' });
 
 /* TODO Passport authentication logic goes here*/
 
+
 /* TODO secure routes go here*/
 
+
+
+// And... start it up!
 app.listen(port, () => {
     console.log(`Running in a ${props.env} environment`);
     console.log(`Server is running in port ${port}.`);
