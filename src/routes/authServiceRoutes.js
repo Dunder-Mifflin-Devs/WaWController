8952c48e8c9fd8e8a0e8799e26a9ac322a51b76b
db556b <davidbuchan900@gmail.com>
2023-10-03 22:25:31 -0400

    removed top-level routes...express did not like that one bit

--- src/routes/authServiceRoutes.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const msRoute = require('../microServices/WaWAuth/authRoutes/authRoutes');
-
-// router.post('/authenticate', msRoute.auth)
-
-// module.exports = router;
--- src/routes/userMgmtRoutes.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const msRoute = require('../microServices/WaWuserManagement/userRoutes/userRoutes');
-
-// // These routes are defined as '/user/
-// router.post('/', msRoute); 
-// router.delete('/', msRoute);
-// router.put('/', msRoute);
-// router.get('/', msRoute);
