d67619611e6c75bf23c22d40e8723bc7c0b6791d
Ryllis Iannotti <conneriannotti@outlook.com>
2023-10-26 15:26:56 -0400

    added two media endpoints added a way to search omdb for specific media or a general list of media added a way to get random media from omdb of a certain type

--- src/microServices/WaWSearch/searchRoutes/searchRoutes.js
+++ src/microServices/WaWSearch/searchRoutes/searchRoutes.js
@@ -1,1 +1,10 @@
-// search routes, duh
+const express = require('express');
+
+require('dotenv').config({ path: '/../../config/.env' });
+const controller = require('../searchControllers/search')
+
+module.exports = (passport) => {
+  const router = express.Router();
+
+  return router;
+};
