cb3941e4ccfbbf71cd6e411f8377420bc2ef879e
C-Iannotti <conneriannotti@outlook.com>
2023-11-04 11:44:21 -0400

    added a put rating endpoint

--- src/microServices/ReviewRatingsService/reviewRatingsRoutes/reviewRatingsRoutes.js
+++ src/microServices/ReviewRatingsService/reviewRatingsRoutes/reviewRatingsRoutes.js
@@ -1,5 +1,5 @@
 const express= require('express');
-const msController= require('../reviewRatingsControllers/reviewsController');
+const rController= require('../reviewRatingsControllers/reviewsController');
 // const ensureAuth= require('pathToAuthFileHere')
 
 // In these routes you will call the controller for the microservice
@@ -8,14 +8,15 @@ module.exports = (passport) => {
     const router= express.Router();
 
     // POST routes
-    router.post('/:id/rating/:rating', msController.postRating) //5 star 
-    router.post('/:id', msController.postReview) //review text
+    router.post('/:id/rating/:rating', rController.postRating) //5 star 
+    router.post('/:id', rController.postReview) //review text
 
     // GET routes
 
     // DELETE routes
 
     // PUT routes
+    router.put("/:id", passport.authenticate("local"), rController.putRating); // update review/rating
 
     return router;
 }
