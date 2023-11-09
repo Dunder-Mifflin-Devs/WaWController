570115baeb10e4ed66a9da7fc75a0d91bb09d2b5
unknown <macrawfo94@gmail.com>
2023-11-08 19:58:26 -0800

    created a single route

--- src/microServices/WaWAuth/authControllers/authController.js
+++ src/microServices/WaWAuth/authControllers/authController.js
@@ -9,20 +9,10 @@ module.exports = {
     auth: function(req,res,next){
 
         },
-    //if the user is authenticated, then they will be successfully logged out and the session destroyed
-    logout: function(req,res,next){
-      if(req.isAuthenticated()){
-        req.logout(()=>{
-            console.log('User has logged out')
-            req.session.destroy(()=>{
-              console.log('Session successfully destroyed')
-              res.send('/')
-            })
-        })
-      }else{
-        console.log("Error : Failed to destroy the session during logout.", err);
-        res.status(205)("/profile");
-        }}
+    // if the user is authenticated, then they will be successfully logged out and the session destroyed
+    logout: function(req,res){
+     
+    }
 }
         
  
