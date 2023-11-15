71a078ae3686daa0c23b37bb45bdcbf63d0740ef
Kevin Borton <kevin.borton@gmail.com>
2023-11-14 19:43:59 -0500

    Updated GetProfile and made the Media Model

--- src/microServices/ReviewRatingsService/reviewRatingsModels/reviewRatingsModels.js
+++ src/microServices/ReviewRatingsService/reviewRatingsModels/reviewRatingsModels.js
@@ -1,1 +1,23 @@
-//
+const mongoose = require('mongoose');
+
+// Schema for combined Reviews and Ratings
+const reviewRatingSchema = new mongoose.Schema({
+    // Common fields for both reviews and ratings
+    userId: mongoose.Schema.Types.ObjectId, // Link to the user who wrote the review or gave the rating
+    mediaId: String, // IMDB ID or similar identifier for the media
+
+    // Rating specific fields
+    rating: {
+        type: Number,
+        required: false // Set to true if you want to enforce rating
+    },
+
+    // Review specific fields
+    reviewContent: {
+        type: String,
+        required: false // Set to true if you want to enforce review text
+    }
+});
+
+// Exporting the ReviewRating model
+module.exports = mongoose.model('ReviewRating', reviewRatingSchema);
