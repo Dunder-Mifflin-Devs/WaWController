85e1568a7ef6a49d3f81064a09be2003b5fc0dd1
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2024-01-17 23:01:47 -0500

    morgan's auth controller code test

--- src/microServices/WaWAuth/authControllers/authController.js
+++ src/microServices/WaWAuth/authControllers/authController.js
@@ -1,7 +1,26 @@
+const passport = require("passport");
+const express = require("express")
+const validator = require("validator");
+const User = require("../../WaWuserManagement/UserModels/User");
+const httpErrors = require('http-errors');
+const session = require("express-session")
+
 
 module.exports = {
-    auth : () => {
-        //auth logic goes here
-    }
-}
+    auth: (req, res) => {
 
+        },
+    logout: (req, res) => {
+      
+      if(req.isAuthenticated()){
+        try {
+          req.session.destroy();
+          res.status(200);
+        } catch (err) { 
+          res.status(503);
+        }
+      } else {
+        res.status(400);
+        }
+      }
+}
